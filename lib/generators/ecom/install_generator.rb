02230840313b552de382b93ae09d452a6f53cd5a
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2014-01-13 07:10:59 +0800

    Updated Route generator

--- lib/generators/ecom/install_generator.rb
+++ lib/generators/ecom/install_generator.rb
@@ -1,11 +1,10 @@
 class Ecom::InstallGenerator < ::Rails::Generators::Base
   include Rails::Generators::Migration
-  source_root File.expand_path('../templates', __FILE__)
   desc "Installs Ecom Store"
 
   def install
     copy_file "../../../../config/locales/devise.en.yml", "config/locales/devise.en.yml"    
     copy_file "../../../../config/locales/en.yml", "config/locales/ecom.en.yml"
-    route "mount Ecom::Engine => "/store""
+    route 'mount Ecom::Engine => "/store"'
   end
 end
