1cf14e319a5e755e1f583dea6728ebdce845b6eb
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2014-01-12 23:36:35 +0800

    Fixed user model

--- app/models/ecom/user.rb
+++ app/models/ecom/user.rb
@@ -35,6 +35,6 @@ module Ecom
   # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
   # field :locked_at,       :type => Time
   
-  has_many :orders, :dependent => destroy
+  has_many :orders, :dependent => :destroy
   end
 end
