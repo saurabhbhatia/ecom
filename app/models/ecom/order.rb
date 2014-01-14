5a51bc4245909a3c1f88d7cd3dbf05d478f71714
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2014-01-14 21:54:51 +0800

    Order scope

--- app/models/ecom/order.rb
+++ app/models/ecom/order.rb
@@ -10,8 +10,8 @@ module Ecom
     has_many :line_items, :dependent => :destroy
     belongs_to :user
     
-    scope :in_progress, where("orders.checked_out_at IS NULL")
-    scope :complete, where("orders.checked_out_at IS NOT NULL")
+    scope :in_progress, where(:checked_out_at.exists => true )
+    scope :complete, where(:checked_out_at => nil )
 
     COMPLETE = "complete"
     IN_PROGRESS = "in_progress"
