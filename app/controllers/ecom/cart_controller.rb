7c08b01ec0eb4c311bb0d4d4aef86ab40a2dcb00
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2014-01-15 19:50:41 +0800

    Fixed typo

--- app/controllers/ecom/cart_controller.rb
+++ app/controllers/ecom/cart_controller.rb
@@ -33,7 +33,7 @@ module Ecom
   protected
 
   def get_cart_value
-    @cart = session[:cart_id] ? Purchase.find(session[:cart_id]) : current_user.purcahses.create(params[:order])
+    @cart = session[:cart_id] ? Purchase.find(session[:cart_id]) : current_user.purchases.create(params[:order])
   end
 
   end
