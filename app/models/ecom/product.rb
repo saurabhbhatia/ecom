5ef17d509e1cfcf463d4c85f96fd16da2ea01382
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2014-01-16 22:24:25 +0800

    Update product model

--- app/models/ecom/product.rb
+++ app/models/ecom/product.rb
@@ -11,5 +11,10 @@ module Ecom
     slug :name, history: true 
     belongs_to :category
     mount_uploader :image, ImageUploader
+    
+    def recalculate_price!
+     self.total_price = line_items.inject(0.0){|sum, line_item| sum += line_item.price }
+     save!
+   end
   end
 end
