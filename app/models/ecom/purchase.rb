d35c4850e5f77f3afe180e0db60daea35e9ed3a5
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2014-01-16 22:20:06 +0800

    Update date

--- app/models/ecom/purchase.rb
+++ app/models/ecom/purchase.rb
@@ -1,11 +1,10 @@
 module Ecom
   class Purchase
     include Mongoid::Document
-    include Mongoid::MultiParameterAttributes
     include Mongoid::Timestamps
     
     field :user_id, type: String
-    field :checked_out_at, type: DateTime   
+    field :checked_out_at, type: Time   
     field :total_price, type: Float
     
     has_many :line_items, :dependent => :destroy
